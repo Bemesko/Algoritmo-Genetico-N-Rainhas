7a2e3856b135e9bfb9a3f356ca918e55c6dc463f
Gregori Sabel <seriosabel57@gmail.com>
2020-09-29 22:53:35 -0300

    babys e parents

--- constants.py
+++ constants.py
@@ -1,11 +1,29 @@
 # Criando constantes
 INDIVIDUAL_AMOUNT = 10
 CROMOSSOME_LENGTH = 8
-REMOVED_INDIVIDUAL_AMOUNT = INDIVIDUAL_AMOUNT - 4
-SURVIVING_INDIVIDUAL_AMOUNT = INDIVIDUAL_AMOUNT - REMOVED_INDIVIDUAL_AMOUNT
+
+SURVIVING_INDIVIDUAL_AMOUNT = 11
+REMOVED_INDIVIDUAL_AMOUNT = INDIVIDUAL_AMOUNT - SURVIVING_INDIVIDUAL_AMOUNT
+
 CROSSING_OVER_INDIVIDUAL_AMOUNT = SURVIVING_INDIVIDUAL_AMOUNT
 CROSSING_OVER_SPLIT_LENGTH = int(CROMOSSOME_LENGTH/2)
 
 # Constantes que não faz diferença mudar
 GENES_KEY = "genes"
 SCORE_KEY = "score"
+
+
+
+# individuos 10
+# mata 50%
+# 50% sobra [lista original so com a metade]
+# os 50% tem filho(25%) [lista de babys]
+# [lista original e manter 10% só]
+# [junta os 35%]
+
+# individuos 10
+# mata 4
+# 6 sobra [lista original so com a metade]
+# os 6 tem filho 3 [lista de babys]
+# [lista original e manter 3 só]
+# [junta os 3 + 7]
--- reproduction.py
+++ reproduction.py
@@ -6,42 +6,49 @@ from constants import *
 
 
 def crossing_over(individuals_list):
-    # criar lista aleatória de indivíduos com os quais acontecerá crossing over
-    crossing_over_individuals = []
     # fazer com que o menor cara fique de certeza nos sobreviventes
-    while len(crossing_over_individuals) < CROSSING_OVER_INDIVIDUAL_AMOUNT:
-        new_individual = individuals_list[random.randrange(
-            0, SURVIVING_INDIVIDUAL_AMOUNT)]
-        if new_individual not in crossing_over_individuals:
-            crossing_over_individuals.append(
-                new_individual)
-    print(crossing_over_individuals)
-    # pegar quantidade aleatória do cromossomo de cada um e criar indivíduo com eles
-    for i in range(int(CROSSING_OVER_INDIVIDUAL_AMOUNT/2)):
-        crossing_over_parents = [
-            crossing_over_individuals[i*2], crossing_over_individuals[i*2+1]]
+    new_generation = []
+    while (len(new_generation) < 10):
+
+        # Declarando BEBE
         crossing_over_baby = {
             GENES_KEY: [],
             SCORE_KEY: 0
         }
+
+        # Selecionando pais sobreviventes aleatorios
+        crossing_over_parents = [0,0]
+        while(crossing_over_parents[0] == crossing_over_parents[1]):
+            crossing_over_parents = [individuals_list[random.randrange(0,SURVIVING_INDIVIDUAL_AMOUNT-1)], 
+                                    individuals_list[random.randrange(0,SURVIVING_INDIVIDUAL_AMOUNT-1)]]
+
+        # Selecionando ponto de split aleatorio
+        cromossome_split = random.randrange(2, CROMOSSOME_LENGTH-3)
+
+        # Fazendo crossing over em si
         for gene in range(CROMOSSOME_LENGTH):
-            if(gene < CROSSING_OVER_SPLIT_LENGTH):
+            if(gene < cromossome_split):
                 crossing_over_baby[GENES_KEY].append(
                     crossing_over_parents[0][GENES_KEY][gene])
             else:
                 crossing_over_baby[GENES_KEY].append(
                     crossing_over_parents[1][GENES_KEY][gene])
-        print("mamai e papai")
-        print(crossing_over_parents[0][GENES_KEY])
-        print(crossing_over_parents[1][GENES_KEY])
+        new_generation.append(crossing_over_baby[GENES_KEY])
+
+        # Prints
+        # print("mamai e papai")
+        # print(crossing_over_parents[0][GENES_KEY])
+        # print(crossing_over_parents[1][GENES_KEY])
         print("Nosso novo neném: ")
         print(crossing_over_baby[GENES_KEY])
+        for i in new_generation:
+            print(i)
+
         # adicionar novos indivíduos na lista de indivíduos
         # individuals_list.append(crossing_over_individuals)
 
-        # Função que
-
-
+        
+# Função que
 def check_if_needs_next_generation(individuals_list):
     print("nova geracao checada!")
 
