5a081bd6f9a9b3b913e16a74b4b1d13733856f4e
Gregori Sabel <seriosabel57@gmail.com>
2020-09-30 10:47:36 -0300

    Penalidade em mesma linha

--- ranking.py
+++ ranking.py
@@ -6,11 +6,24 @@ from constants import *
 
 # Função que ordena e calcula a penalidade do individuo
 def score_individual(individual_chromossome):
+
+    penalty = 0
+
     clone_a = copy.deepcopy(individual_chromossome)
     clone_b = copy.deepcopy(individual_chromossome)
-    penalty = 0
+    clone_reto = copy.deepcopy(individual_chromossome)
+
+    # Calculando penalidade de gente na mesma linha
+    clone_reto.sort()
+    for i in range(len(clone_reto)-1):
+        if (i == i+1):
+            penalty += 1
+
+    # Calculando diagonal \
     order_list(clone_a, 1)
     penalty = calculate_penalty(clone_a, penalty)
+
+    # Calculando diagonal /
     order_list(clone_b, -1)
     penalty = calculate_penalty(clone_b, penalty)
 
@@ -48,4 +61,10 @@ def kill_worst_individuals(individuals_list):
 
 
 if __name__ == "__main__":
-    pass
+    from graphics import *
+    from constants import *
+
+    lista_teste = [[2, 5, 4, 3, 7, 6, 0, 7], [7, 3, 0, 1, 6, 4, 5, 2], [3, 5, 6, 4, 7, 2, 1, 0], [6, 5, 1, 3, 7, 0, 4, 2], [0, 1, 5, 4, 6, 7, 3, 2]]
+
+    penalidade = score_individual(lista_teste[0])
+    plot_to_board(lista_teste[0], penalidade)
